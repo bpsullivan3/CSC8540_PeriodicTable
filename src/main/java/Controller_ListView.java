4c939a7e328d16129cf611a7382b1183c92b2511
17kpatrick <43053006+17kpatrick@users.noreply.github.com>
2018-09-25 12:45:51 -0400

    Periodic Table User Interface V .01
    Signed-off-by: 17kpatrick <43053006+17kpatrick@users.noreply.github.com>

---
+++ src/main/java/Controller_ListView.java
@@ -0,0 +1,15 @@
+package main.java;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller_ListView implements Initializable
+{
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+
+    }
+}
